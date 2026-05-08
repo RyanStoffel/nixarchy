d727de30b8d52e12163bbd1ad75a96db564d6d0f
RyanStoffel <ryanstoffel62@icloud.com>
2026-05-08 13:46:23 -0700

    feat(modules): add hyprland desktop module

---
+++ modules/desktop/hyprland.nix
@@ -0,0 +1,17 @@
+{ inputs, pkgs, ... }:
+let
+  system = pkgs.stdenv.hostPlatform.system;
+in {
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  security.polkit.enable = true;
+}
